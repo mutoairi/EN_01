abd2f3150b1934a7ce74a0559b352fc04e23ce1c
愛里 武藤 <K023G0026@g.neec.ac.jp>
2024-04-17 15:18:42 +0900

    hennkou

--- Assets/GameManagerScript2.cs
+++ Assets/GameManagerScript2.cs
@@ -6,13 +6,64 @@ public class GameManagerScript2 : MonoBehaviour
 {
     //配列の宣言
     int[] map;
+    void PrintArray()
+    {
+        //追加。文字列の宣言と初期化
+        string debugText = "";
+
+        for (int i = 0; i < map.Length; i++)
+        {
+            //変更。文字列に結合していく
+            debugText += map[i].ToString() + ",";
+        }
+
+        Debug.Log(debugText);
+    }
+    int GetPlayerIndex() { 
+    for(int i = 0; i = map.Length; i++)
+        {
+            if (map[i] == 1)
+            {
+                return i;
+            }
+
+        }
+        return -1;
+    }
+    //クラスの中メソッドの外に書くことに注意
+    //返り値の型に注意
+    bool MoveNumber(int number,int moveFrom,int moveTo)
+    {
+        //移動先が範囲外なら移動不可
+
+        if (moveTo < 0 || maveTo >= map.Length)
+        {
+            //動けない条件を先に書きリターンする＝早期リターン
+            return false;
+        }
+        //移動先に2(箱)が居たら
+        if (map[moveTo] == 2)
+        {
+            //どの方向へ移動するかを算出
+            int velocity = moveTo - moveFrom;
+
+            //プライヤーの移動先からさらに先へ2(箱)を移動させる
+            //
+        }
+     
 
 
+
+            map[moveTo] = number;
+        map[moveFrom] = 0;
+        return true;
+    }
     // Start is called before the first frame update
     void Start()
     {
         //配列の実態の作成と初期化
-        map = new int[] { 0, 0, 0, 1, 0, 0, 0, 0, 0 };
+        map = new int[] { 0, 0, 0, 1, 0, 2, 0, 0, 0 };
+        PrintArray();
 
 
     }
@@ -22,36 +73,14 @@ public class GameManagerScript2 : MonoBehaviour
     {
         if (input.GetKeyDown(KeyCode.RightArrow))
         {
-            //playerIndex
-            int playerIndex = -1;
-            for (int i = 0; i < map.Length; i++)
-            {
-                if (map[i] == 1)
-                {
-                    playerIndex = i;
-                    break;
-                }
-            }
-            /*
-             playerIndex+1のインデックスのものと交換するので
-            playerIndex-1よりさらに小さいインデックスの時のみ交換処理を行う
-             */
-            if (playerIndex < map.Lengh - 1)
-            {
-                map[playerIndex + 1] = 1;
-                map[playerIndex] = 0;
-            }
-            //追加。文字列の宣言と初期化
-            string debugText = "";
+            //メソッド化した処理を使用
 
-            for (int i = 0; i < map.Length; i++)
-            {
-                //変更。文字列に結合していく
-                debugText += map[i].ToString() + ",";
-            }
+            int playerIndex = GetPlayerIndex();
+            //移動処理を関数化
+            MoveNumber(1, playerIndex, playerIndex + 1);
 
-            Debug.Log(debugText);
 
-        };
-    }
+            PrintArray();
+
+        }
 }
